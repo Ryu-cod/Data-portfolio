60177539033d050fced772d3cd2c85e0c8aff93d
Ryu-cod <timmymwaniki@gmail.com>
2026-01-28 14:39:32 -0500

    Update 02_insert_data.sql.sql

---
+++ data-portfolio/SQL/02_insert_data.sql.sql
@@ -0,0 +1,26 @@
+-- ============================================
+-- 02_insert_data.sql
+-- Insert Employee Records
+-- ============================================
+
+INSERT INTO employees (employee_id, first_name, last_name, age, gender, department, job_title, employment_type, salary, hire_date, location) VALUES
+(2001,'AnnaLisa','Peterson',43,'Female','IT / Data','Data Engineer','Full time',110500,'2015-03-12','New York NY'),
+(2002,'Barbra','Jean',32,'Female','Administration','Secretary','Full time',55600,'2018-07-22','New York NY'),
+(2003,'Ruth','Mwalugha',26,'Female','IT Support','Tech Support','Full time',76900,'2020-11-03','Jersey City NJ'),
+(2004,'Peter','Jones',31,'Male','IT','Information Technology Assistant','Full time',85600,'2017-01-15','Jersey City NJ'),
+(2005,'Patel','Singh',25,'Male','IT','Information Management Assistant','Full time',87640,'2021-06-09','Boston MA'),
+(2006,'Beatrice','Garcia',52,'Female','Human Resources','HR','Full time',83546,'2012-09-18','Boston MA'),
+(2007,'Francois','Cortez',30,'Male','Finance','Accountant','Full time',82600,'2019-04-27','Boston MA'),
+(2008,'Ali','Mohammed',28,'Male','Legal','Legal Counsel','Full time',120150,'2016-02-10','Boston MA'),
+(2009,'David','Njia Jr',33,'Male','Operations','Regional Manager','Full time',98890,'2014-08-05','Boston MA'),
+(2010,'John','Navallo',55,'Male','Executive','CEO','Full time',354002,'2010-01-01','Jersey City NJ'),
+(2011,'Hussein','Maini',49,'Male','Finance / IR','Investors Relationship Specialist','Full time',84100,'2013-10-14','Jersey City NJ'),
+(2012,'Margaret','Blessing',44,'Female','Executive','CFO','Full time',299800,'2011-05-03','Jersey City NJ'),
+(2013,'Gladys','Tokar',38,'Female','Data','Data Analyst','Full time',88950,'2016-12-19','Jersey City NJ'),
+(2014,'Vladimir','Medevez',27,'Male','Marketing','Marketing Manager','Full time',97880,'2022-04-11','Jersey City NJ'),
+(2015,'Susan','Hao',30,'Female','IT / Engineering','Software Engineer','Full time',118500,'2018-09-07','New York NY'),
+(2016,'Patricia','Lopez',51,'Female','IT Infrastructure','System Administrator','Full time',101560,'2011-03-25','New York NY'),
+(2017,'Billy','Spiller',41,'Male','Cloud / IT','Cloud Architect','Full time',98700,'2015-06-16','New York NY'),
+(2018,'Hadija','Fatma',42,'Female','Finance','Controller','Full time',119500,'2017-10-02','New York NY'),
+(2019,'Keith','Kamau',36,'Male','Administration','Office Manager','Full time',78560,'2019-02-21','New York NY'),
+(2020,'Faith','Nkrumah',37,'Female','IT Support','Tech Support','Full time',76900,'2020-05-14','Jersey City NJ');
